edd59d7f25e7b7d4baa8094aad5a73bae1f601d2
unknown <kaustubhkrishna9031@gmail.com>
2020-01-02 23:09:20 +0530

    allowed hosts

--- djangoFitness/settings.py
+++ djangoFitness/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-!#m^rmki3d+0!yjk8r4mk92sy+u+a3vpzr@fkbzw$8=3^s979'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
