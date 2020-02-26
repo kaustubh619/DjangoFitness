d2faaaf8602261682a0d7593db9d68d24797eb40
unknown <kaustubhkrishna9031@gmail.com>
2020-02-26 17:44:12 +0530

    now

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('blog_categories', views.BlogCategoryView.as_view({'get': 'cat_list'})),
-    path('blog_post/', views.BlogPostView.as_view(), name="blog_post$"),
+    path('blog_post', views.BlogPostView.as_view(), name="blog_post$"),
     path('blog/', views.BlogPostViewWithoutPagination.as_view(), name="blog_post$"),
     path('blogbycategory/<int:pk>', views.BlogByCategory.as_view()),
     path('blogbyid/<slug:slug>', views.BlogById.as_view()),
